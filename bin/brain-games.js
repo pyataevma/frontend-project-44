18ce8d629a5661507b8345f66f401aadbfaf61a1
Mikhail Pyataev <pyataevma@gmail.com>
2022-11-23 16:00:40 +0600

    added eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,16 +1,5 @@
 #!/usr/bin/env node
 
-import { greeting } from "../src/cli.js";
-// import readlineSync from 'readline-sync';
-
-// console.log('Welcome to the Brain Games!');
-
-// const greeting = () => {
-//     console.log('brain-games');
-//     console.log('Welcome to the Brain Games!');
-//     const userName = readlineSync.question('May I have your name? ');
-//     //May I have your name? John
-//     console.log(`Hello, ${userName}!`);
-// }
+import greeting from '../src/cli.js';
 
 greeting();
